3379ee77b13563d40b62437cfaf173ce5608fcee
erdmko <erdmko@gmail.com>
2015-07-14 20:41:48 +0300

    Css concatinate and retab

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,88 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
+grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-      vendor: {
-        src: [
-          'bower_components/intro.js/minified/intro.min.js',
-          'bower_components/underscore/underscore-min.js',
-          'bower_components/moment/min/moment.min.js',
-          'bower_components/spin.js/spin.js',
-          'bower_components/angular/angular.min.js',
-          'bower_components/angular-route/angular-route.min.js',
-          'bower_components/angular-animate/angular-animate.min.js',
-          'bower_components/elasticsearch/elasticsearch.angular.min.js',
-          'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-          'bower_components/angular-scroll-glue/src/scrollglue.js',
-          'bower_components/angular-loading-bar/build/loading-bar.min.js',
-          'bower_components/angular-spinner/angular-spinner.min.js'
-        ],
-        dest: 'build/vendor.min.js'
-      },
-      elv: {
-        options : {
-            sourceMap :true
+        vendor_css: {
+            src: [
+                'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                'bower_components/intro.js/minified/introjs.min.css',
+                'bower_components/angular-loading-bar/build/loading-bar.min.css',
+            ],
+            dest: 'build/css/vendor.min.css'
         },
-        src: [
-          'js/*.js'
-        ],
-        dest: '.tmp/<%= pkg.name %>.js'
-      }
+        app_css: {
+            src: [
+                'css/*.css'
+            ],
+            dest: 'build/css/<%= pkg.name %>.css'
+        },
+        vendor: {
+            src: [
+                'bower_components/intro.js/minified/intro.min.js',
+                'bower_components/underscore/underscore-min.js',
+                'bower_components/moment/min/moment.min.js',
+                'bower_components/spin.js/spin.js',
+                'bower_components/angular/angular.min.js',
+                'bower_components/angular-route/angular-route.min.js',
+                'bower_components/angular-animate/angular-animate.min.js',
+                'bower_components/elasticsearch/elasticsearch.angular.min.js',
+                'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+                'bower_components/angular-scroll-glue/src/scrollglue.js',
+                'bower_components/angular-loading-bar/build/loading-bar.min.js',
+                'bower_components/angular-spinner/angular-spinner.min.js',
+            ],
+            dest: 'build/js/vendor.min.js'
+        },
+        elv: {
+            options : {
+                sourceMap :true
+            },
+            src: [
+                'js/*.js'
+            ],
+            dest: '.tmp/<%= pkg.name %>.js'
+        }
+    },
+    copy: {
+        fonts: {
+            expand: true,
+            dot: true,
+            cwd: 'bower_components/bootstrap/fonts/',
+            src: '*',
+            dest: 'build/fonts/',
+        },  
     },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        sourceMap : true,
-        sourceMapIncludeSources : true,
-        sourceMapIn : '.tmp/<%= pkg.name %>.js.map'
-      },
-      build: {
-        files: {
-          'build/<%= pkg.name %>.js': ['js/*.js']
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+            sourceMap : true,
+            sourceMapIncludeSources : true,
+            sourceMapIn : '.tmp/<%= pkg.name %>.js.map'
+        },
+        build: {
+            files: {
+                'build/js/<%= pkg.name %>.js': ['js/*.js']
+            }
         }
-      }
     },
     watch: {
-      scripts: {
-        files: ['**/*.js', '!build/*.js',
+        scripts: {
+            files: [
+                '**/*.js',
+                '!build/**',
                 '!bower_components/**/*.js',
-                '!node_modules/**/*.js'],
-        tasks: ['concat', 'uglify']
-      }
+                '!node_modules/**/*.js'
+            ],
+            tasks: ['concat', 'uglify']
+        }
     }
-  });
+});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['concat', 'copy', 'uglify']);
 };
